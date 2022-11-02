eebc07ba3ec3a7d9ff09377f1a7b9ead5cc6d3a4
Victoria Maggay <toria0305@gmail.com>
2022-11-01 23:48:31 -0400

    commit tree ae714b3512be8e700f16d2df14507e9a96f0fe0b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName()  {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Vic'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'maybe Vic'
+}
